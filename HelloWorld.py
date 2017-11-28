8ddec693108c6be93bd8020a52ba32b9bda7d822
loesxje <loesmodderman@gmail.com>
2017-11-28 15:05:35 +0100

    3:05 28/11/2017

---
+++ HelloWorld.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib as mplot
+
+print('Hello World')
+
+
---
+++ read_YAML_and_Camera.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib as mplot
+import yaml
+import cv2
+
+
+# read YML file.
+with open("ueyecallib.yml", 'r') as stream:
+    try:
+        print(yaml.load(stream))
+    except yaml.YAMLError as exc:
+        print(exc)
+
+# read camera images
+cap = cv2.VideoCapture(0)
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # Display the resulting frame
+    cv2. imshow('frame', gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything is done, release the capture
+cap.release()
+cv2.destroyAllWindows
