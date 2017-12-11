1b027202969620f760a4da0702f9e365bc521090
Simcha <simchavanhelvoort@gmail.com>
2017-12-11 12:34:37 +0100

    fixed read camera

--- read_YAML_and_Camera.py
+++ read_YAML_and_Camera.py
@@ -3,13 +3,15 @@ import matplotlib as mplot
 import yaml
 import cv2
 
+doCallibrate = False
 
-# read YML file.
-with open("ueyecallib.yml", 'r') as stream:
-    try:
-        print(yaml.load(stream))
-    except yaml.YAMLError as exc:
-        print(exc)
+if doCallibrate:
+    # read YML file.
+    with open("ueyecallib.yml", 'r') as stream:
+        try:
+            print(yaml.load(stream))
+        except yaml.YAMLError as exc:
+            print(exc)
 
 # read camera images
 cap = cv2.VideoCapture(0)
@@ -17,15 +19,18 @@ cap = cv2.VideoCapture(0)
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
+    if not ret:
+        raise ValueError("Failed to load frames")
     # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    grayImage = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     # Display the resulting frame
-    cv2. imshow('frame', gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    cv2. imshow('frame', grayImage)
+    key = cv2.waitKey(1) # Break when spacebar is pressed
+    if key == 32:
         break
-cv2.imwrite("frameSaveTest.jpg", gray)
+
+cv2.imwrite("frameSaveTest.jpg", grayImage)
 
 # When everything is done, release the capture
 cap.release()
-
-cv2.destroyAllWindows
+cv2.destroyAllWindows()
