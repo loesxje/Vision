e09e4b1c34258df6f836ab150411a972d72b811f
evaamol <33519017+evaamol@users.noreply.github.com>
2017-12-19 20:57:53 +0100

    NIEUWE

---
+++ trainingProgram.py
@@ -0,0 +1,91 @@
+import cv2
+import numpy as np
+import avlBPNEvaWorking as BPN
+import avansvisionlib as avl
+import pandas as pd
+import os
+import extractFeatures as ef
+
+def trainHandwrittenNumbers(imageWD):
+
+    # Maximale fout die toegestaan wordt in de output voor de training input
+    MAX_OUTPUT_ERROR = 1E-8
+    # maximaal aantal runs dat uitgevoerd wordt bij het trainen
+    MAXRUNS = 10000
+
+    imageCodes = []
+
+    for file in os.listdir(imageWD):  # +folder
+        if file != ".DS_Store":
+            numberOfInputs = len(os.listdir(imageWD))
+            imageCodes.append(file)
+        image = ef.makeBinaryImage(imageWD + file)
+
+    # print(imageCodes)
+    # avl.show16SImageStretch(image, "image")
+
+    numberOfFeatures = image.shape[0] * image.shape[1] + 1
+    numberOfOutputs = 4
+
+    print("\n Testset laden... \n \n")
+    print("\t BPN Inputlayer  = " + str(numberOfInputs) + " neurons")
+    print("\t BPN Outputlayer = " + str(numberOfOutputs) + " neurons")
+    hiddenNeurons = int(input("Please choose a number of hidden neurons: "))
+    print("Thank You!!")
+
+    print("Initialize BPN... ")
+
+    V0, W0, dV0, dW0 = BPN.initializeBPN(numberOfFeatures, hiddenNeurons, numberOfOutputs)
+
+    #perimeterMax, areaMax = ef.memoriseLargest(imageWD)
+    counter = 0
+
+    # Haal de afbeeldingen uit de map.
+    for i in range(len(imageCodes)):
+        # TODO: random aanpassen
+        counter += 1
+        print(counter)
+        indexnummer = np.random.randint(len(imageCodes))
+        filename = imageCodes.pop(indexnummer)
+        print(filename)
+        binaryImage = ef.makeBinaryImage(imageWD + filename)
+
+        runs = 0
+
+        sumSqrDiffError = MAX_OUTPUT_ERROR + 1
+        # looping over afbeeldingen met eenen,tweeen drieen, vieren, vijfen, zessen enz
+        # Voor elke feature worden de weights bepaald
+
+        while ((sumSqrDiffError > MAX_OUTPUT_ERROR) & (runs < MAXRUNS)):
+            sumSqrDiffError = 0
+            #     for inputSetRowNr in range(ITset.shape[0]):  # Afbeeldingen in de map
+            # for inputSetRowNr in range(binaryImage.shape[0]):
+            IT = np.array(ef.extractFeatures(binaryImage))
+            OT = np.array(ef.outputHandwrittenNumbers(filename))
+            OH = BPN.calculateOutputHiddenLayer(IT, V0)
+            OO = BPN.calculateOutputBPN(OH, W0)
+            [V1, W1] = BPN.adaptVW(OT, OO, OH, IT, W0, dW0, V0, dV0)
+            outputError0 = BPN.calculateOutputBPNError(OO, OT)
+            outputError1 = BPN.calculateOutputBPNError(BPN.BPN(IT, V1, W1), OT)
+            sumSqrDiffError += (outputError1 - outputError0) * (outputError1 - outputError0)
+            V0 = V1
+            W0 = W1
+            #print("sumSqrDiffError = " + str(sumSqrDiffError))
+            runs += 1
+        print("Runs = " + str(runs))
+    # Print de output
+    # outputVectorBPN = OTset.copy()
+    # for inputSetRowNr in range(ITset.shape[0]):
+    #     for inputSetColNr in range(OTset.shape[1]):
+    #         inputVectorTrainingSet = ITset[inputSetRowNr]
+    #         outputVectorBPN[inputSetRowNr][inputSetColNr] = round(round(BPN.BPN(inputVectorTrainingSet, V0, W0)[inputSetColNr][0]), 1)
+    #
+    print("BPN Training is ready!")
+    print(V0)
+    print(W0)
+    return V0, W0
+
+    # outputTraining = pd.DataFrame(outputVectorBPN)
+    # outputTraining.columns = ["Output BPN"]
+    # originalSet[outputTraining.columns] = outputTraining
+    # print(originalSet)
